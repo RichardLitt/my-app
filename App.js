d4b0da63c2d43a5736db6d80f2cd3a26c125491e
Richard Littauer <richard.littauer@gmail.com>
2017-12-08 17:45:24 -0500

    Select a photo and display it

--- App.js
+++ App.js
@@ -15,6 +15,9 @@ import {
   RefreshControl
 } from 'react-native'
 
+const imagemin = require('imagemin');
+const imageminPngquant = require('imagemin-pngquant');
+
 // import RNFetchBlob from 'react-native-fetch-blob'
 
 let styles
@@ -22,13 +25,28 @@ const { width } = Dimensions.get('window')
 
 class App extends React.Component {
   static navigationOptions = {
-    title: 'Camera Roll App',
+    title: 'Suhaili',
+  }
+
+  getImage = (index) => {
+    this.setState({ chunkImage: this.state.photos[this.state.index].node.image})
+  }
+
+  chunk = (index) => {
+    this.setState({ modalVisible: !this.state.modalVisible });
+    this.setState({ chunk: !this.state.chunk });
+  }
+
+  chunkIt = () => {
+    console.log('Fuck yeah')
   }
 
   state = {
     modalVisible: false,
     photos: [],
-    index: null
+    index: null,
+    chunk: false,
+    chunkImage: {}
   }
 
   setIndex = (index) => {
@@ -57,58 +75,89 @@ class App extends React.Component {
 
   render() {
     console.log('state :', this.state)
+    // console.log('index :', this.index)
     return (
       <View style={styles.container}>
         <Button
           title='View Photos'
           onPress={() => { this.toggleModal(); this.getPhotos() }}
         />
-        <Button
-          title='Browse Images'
-          onPress={this.navigate}
-        />
-        <Modal
-          animationType={"slide"}
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestClose={() => console.log('closed')}
-        >
-          <View style={styles.modalContainer}>
-            <Button
-              title='Close'
-              onPress={this.toggleModal}
-            />
-            <ScrollView
-              contentContainerStyle={styles.scrollView}>
+          <Modal
+            animationType={"none"}
+            transparent={false}
+            visible={this.state.chunk}
+            onRequestClose={() => {
+              this.state.chunk = false
+              console.log('closed')
+            }}
+          >
+            <View style={styles.modalContainer}>
+              <Button
+                title='Close'
+                onPress={this.chunk}
+              />
+              <Image
+                style={{
+                  width: width,
+                  height: width
+                }}
+                source={{uri: this.state.chunkImage.uri}}
+              ></Image>
+              <Text
+                style={styles.textField}
+              >This is the image which will be split into various files.</Text>
+              <Button
+                title='Chunk It!'
+                onPress={this.chunkIt}
+              />
+            </View>
+          </Modal>
+          <Modal
+            animationType={"slide"}
+            transparent={false}
+            visible={this.state.modalVisible}
+            onRequestClose={() =>  console.log('closed')}
+          >
+            <View style={styles.modalContainer}>
+              <Button
+                title='Close'
+                onPress={this.toggleModal}
+              />
+              <Button
+                title='Chunk'
+                onPress={() => { this.getImage(this.state.index); this.chunk() }}
+              />
+              <ScrollView
+                contentContainerStyle={styles.scrollView}>
+                {
+                  this.state.photos.map((p, i) => {
+                    return (
+                      <TouchableHighlight
+                        style={{opacity: i === this.state.index ? 0.5 : 1}}
+                        key={i}
+                        underlayColor='transparent'
+                        onPress={() => this.setIndex(i)}
+                      >
+                        <Image
+                          style={{
+                            width: width/3,
+                            height: width/3
+                          }}
+                          source={{uri: p.node.image.uri}}
+                        />
+                      </TouchableHighlight>
+                    )
+                  })
+                }
+              </ScrollView>
               {
-                this.state.photos.map((p, i) => {
-                  return (
-                    <TouchableHighlight
-                      style={{opacity: i === this.state.index ? 0.5 : 1}}
-                      key={i}
-                      underlayColor='transparent'
-                      onPress={() => this.setIndex(i)}
-                    >
-                      <Image
-                        style={{
-                          width: width/3,
-                          height: width/3
-                        }}
-                        source={{uri: p.node.image.uri}}
-                      />
-                    </TouchableHighlight>
-                  )
-                })
+                this.state.index !== null  && (
+                  <View style={styles.shareButton}>
+                  </View>
+                )
               }
-            </ScrollView>
-            {
-              this.state.index !== null  && (
-                <View style={styles.shareButton}>
-                </View>
-              )
-            }
-          </View>
-        </Modal>
+            </View>
+          </Modal>
       </View>
     )
   }
@@ -134,6 +183,10 @@ styles = StyleSheet.create({
     padding: 10,
     bottom: 0,
     left: 0
+  },
+  textField: {
+    justifyContent: 'center',
+    padding: 10
   }
 })
 
